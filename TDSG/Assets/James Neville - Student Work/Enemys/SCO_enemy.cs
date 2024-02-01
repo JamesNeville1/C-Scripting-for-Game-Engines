d8068ffb8b53565adc9e47ee955e9e14d3234afc
Izzet <jamesliamneville@gmail.com>
2024-02-01 10:52:11 +0000

    let extra

---
+++ TDSG/Assets/James Neville - Student Work/Enemys/SCO_enemy.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SCO_item_useable_health_", menuName = "ScriptableObjects/Items/Useable/DamageOrHeal")]
+public class SCO_enemy : ScriptableObject {
+    
+}
---
+++ TDSG/Assets/James Neville - Student Work/Scripts/SCR_overworld_enemy.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_overworld_enemy : MonoBehaviour {
+    private SpriteRenderer sr;
+    private SCR_entity_animation enemyAnimator;
+}
