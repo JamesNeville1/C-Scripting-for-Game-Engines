239026e0b750b8e549336d394758e1762d518d41
Izzet <jamesliamneville@gmail.com>
2024-03-07 13:45:28 +0000

    weapons research

--- TDSG/Assets/James Neville - Student Work/Items/Logic/Base Logic/SCO_item_modular_weapon.cs
+++ TDSG/Assets/James Neville - Student Work/Items/Logic/Base Logic/SCO_item_modular_weapon.cs
@@ -1,25 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "SCO_item_weapon_", menuName = "ScriptableObjects/Items/Weapons")]
-public class SCO_item_modular_weapon : SCO_item{
-    [SerializeField] private UnityEvent<aaaaa> onLeftClick ;
-    [SerializeField] private UnityEvent onRightClick;
-
-    [System.Serializable]
-    public struct aaaaa {
-        [SerializeField] public int a;
-        [SerializeField] public int b;
-    }
-
-    public void a(aaaaa a) {
-
-    }
-    public void b() { 
+public class SCO_item_modular_weapon : SCO_item {
+    [Header("Universal Weapon Vars")]
+    [SerializeField] private int damage;
     
-    }
-
+    [Header("Weapon Events")]
+    [SerializeField] private UnityEvent onLeftClick;
+    [SerializeField] private UnityEvent onRightClick;
 }
